6b198176d854661942eb167764e37877dd9251ec
sqren@mail.ustc.edu.cn <sqren@mail.ustc.edu.cn>
2015-09-22 12:38:08 +0800

    add default path for caffe

--- utils/active_caffe_mex.m
+++ utils/active_caffe_mex.m
@@ -14,6 +14,13 @@ function active_caffe_mex(gpu_id, caffe_version)
     end
     cur_dir = pwd;
     caffe_dir = fullfile(pwd, 'external', 'caffe', 'matlab', caffe_version);
+    
+    if ~exist(caffe_dir, 'dir')
+        warning('Specified caffe folder (%s) is not exist, change to default one (%s)', ...
+            caffe_dir, fullfile(pwd, 'external', 'caffe', 'matlab'));
+        caffe_dir = fullfile(pwd, 'external', 'caffe', 'matlab');
+    end
+    
     addpath(genpath(caffe_dir));
     cd(caffe_dir);
     caffe.set_device(gpu_id-1);
